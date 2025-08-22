f4a5db42729b9c8f8bad7d366d0081b434274082
Radu - Florin Tataru <Radu-Florin.Tataru@ing.com>
2025-08-22 15:00:23 +0300

    typo in filepath

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String fileType = "json"; // Change to "txt" or "csv" to test other readers
-        String filePath = "data/websites." + fileType;
+        String filePath = "resources/websites." + fileType;
 
         FileReader reader = FileReaderFactory.createReader(fileType);
         WebsiteOpener opener = WebsiteOpener.getInstance();
